a8b50998982b5b5bb89d107170f1fc76a4a80838
Alexandra S <alexandrastanjic@gmail.com>
2021-04-06 21:36:00 +0300

    get weather API and update the location

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -1,1 +1,26 @@
-const key = '	8TMAZ9wkH2KoGBAae24KUnPlFlcEo0Nb';
+const key = 'odxr8ubrkw5ZGht4NonKG03GECi79rzT';
+
+const getWeather = async (id) => {
+
+ const base =  'http://dataservice.accuweather.com/currentconditions/v1/';
+ const query = `${id}?apikey=${key}`;
+
+ const response = await fetch(base + query);
+ const data = await response.json();
+
+ return (data[0]);
+};
+
+const getCity = async (city) => {
+
+  const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+  const query = `?apikey=${key}&q=${city}`;
+
+  const response = await fetch(base + query);
+  const data = await response.json();
+
+  return (data[0])
+
+};
+
+
