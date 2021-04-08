ad4f1227e4365d057c466d738598621ef4f887ce
Alexandra S <alexandrastanjic@gmail.com>
2021-04-08 14:37:56 +0300

    Add weather img & icons - finish the app

--- scripts/app.js
+++ scripts/app.js
@@ -1,14 +1,43 @@
 const cityForm  = document.querySelector ('form');
+const card = document.querySelector('.card');
+const details = document.querySelector('.details');
+const time = document.querySelector('img.time');
+const icon = document.querySelector ('.icon img');
+
+const updateUI = (data) => {
+
+  const { cityDets, weather} = data;
+
+  details.innerHTML = `
+    <h5 class="my-3">${cityDets.EnglishName}</h5>
+    <div class="my-3">${weather.WeatherText}</div>
+    <div class="display-4 my-4">
+      <span>${weather.Temperature.Metric.Value}</span>
+      <span>&deg;C</span>
+    </div>`;
+
+    const iconSrc = `img/icons/${weather.WeatherIcon}.svg`;
+    icon.setAttribute('src', iconSrc); 
+
+    let timeSrc = null;
+    if(weather.IsDayTime) {
+      timeSrc = 'img/day.svg'
+    } else {
+      timeSrc = 'img/night.svg'
+    }
+    time.setAttribute('src', timeSrc)
+
+    if(card.classList.contains('d-none')) {
+      card.classList.remove('d-none');
+    }
+}
 
 const updateCity = async (city) => {
 
    const cityDets = await getCity(city);
-   const weather = await getWeather(cityDets.key);
+   const weather = await getWeather(cityDets.Key);
 
-   return {
-    cityDets: cityDets,
-    weather: weather
-   };
+   return { cityDets, weather };
 
 };
 
@@ -19,6 +48,6 @@ cityForm.addEventListener('submit', e => {
   cityForm.reset();
 
   updateCity(city)
-  .then(data => console.log(data))
+  .then(data => updateUI(data))
   .catch(err => console.log(err));
 });
